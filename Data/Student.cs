d632d207bfdfab29ccefa016421a143bc53f5bef
Achuth Chandra Moonnamkuttiyil <achuth@umd.edu>
2024-10-14 12:47:59 -0400

    Updated UI elements

--- Data/Student.cs
+++ Data/Student.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Edusync.Data;
 
 public partial class Student
 {
     public int Id { get; set; }
-
+    
     public string FirstName { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
